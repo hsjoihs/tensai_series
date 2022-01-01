d2cc8b48943ade0743104d46dc8269e19c71ce9a
hsjoihs <hs.ioling.hs@gmail.com>
2022-01-01 21:28:09 +0900

    Add files via upload

---
+++ 回路の天才@/genelec.files/betrow.js
@@ -0,0 +1,78 @@
+﻿function thsRowUp()
+{
+	if(xp*yp==0){alert("マスが指定されていないか、枠線が出ていないので編集できません。");return;}
+	var xx="",m=0;
+	
+	for(m=0;m<colu;m++)
+	{
+		xx+="blnk";
+	}
+	var daat=Save(false)//ユーザーに聞かない
+	rw++;
+	daat=daat.split(",");
+	daat.splice(yp-1,0,xx);
+	daat=daat+"";
+	Imp(daat,false);
+	
+
+}
+function thsRowDn()
+{
+	if(xp*yp==0){alert("マスが指定されていないか、枠線が出ていないので編集できません。");return;}
+	if(rw<2){alert("残り1行なので削除できません。");return;}
+	if(confirm("本当に行を削除してよろしいですか？"))
+	{
+		
+		var daat=Save(false)//ユーザーに聞かない
+		rw--;
+		daat=daat.split(",");
+		daat[yp-1]="";
+		daat=daat+"";
+		daat=daat.replace(",,",",");
+		Imp(daat,false);
+	}
+}
+function thsColUp()
+{
+	if(xp*yp==0){alert("マスが指定されていないか、枠線が出ていないので編集できません。");return;}
+	var daat=Save(false)//ユーザーに聞かない
+	daat=daat.split(",");
+	//xp
+	var n;
+	colu++;
+	for(n in daat)
+	{
+		daat[n]=daat[n].substr(0,4*xp-4)+"blnk"+daat[n].substr(4*xp-4,4*colu-3);
+		if(daat[n]=="blnk"){daat[n]="";}//空行
+	}
+	daat=daat+"";
+	Imp(daat,false);
+	
+}
+function thsColDn()
+{
+	if(xp*yp==0){alert("マスが指定されていないか、枠線が出ていないので編集できません。");return;}
+	if(colu<2)
+	{
+		alert("残り1列なので削除できません。");return;
+	}
+	
+	if(confirm("本当に列を削除してよろしいですか？"))
+	{
+		
+		var daat=Save(false);//ユーザーに聞かない
+		colu--;
+		daat=daat.split(",");
+		//xp
+		var n;
+		for(n in daat)
+		{
+			daat[n]=daat[n].substr(0,4*xp-4)+daat[n].substr(4*xp,4*colu+5);
+		}
+		daat=daat+"";
+		
+	//	alert(daat);
+		Imp(daat,false);
+		
+	}
+}
---
+++ 回路の天才@/genelec.files/save.js
@@ -0,0 +1,101 @@
+﻿function Save(cnf)
+{
+	var Data=document.getElementById("Crc").innerHTML.toLowerCase();
+	alert(1+","+Data);
+	var exp="";
+	var i;
+	var tmp=[];
+	var Tmp=0;
+	Data=Data.replace(/\<tbody\>/g,"").replace(/\<\/tbody\>/g,"");
+	Data=Data.replace(/\<table.+?\>/g,"").replace(/\<\/table\>/g,"");
+	Data=Data.replace(/\<\/tr\>/g,"");
+	Data=Data.replace(/genelec.files\//g,"").replace(/\.png/g,"").replace(/\<\/td\>/g,"");
+	Data=Data.replace(/height=\"..\"\s/g,"");
+	Data=Data.replace(/width=\"..\"/g,"");
+	Data=Data.replace(/\<td.+?\>/g,"");
+	Data=Data.replace(/\<tr.+?\>/g,"");
+	Data=Data.replace(/onclick\=/g,"");
+	Data=Data.replace(/img/g,"").replace(/src/g,"");
+	Data=Data.replace(/\<\s\=/g,"");
+	Data=Data.replace(/\"path/g,"");
+	Data=Data.replace(/\)\"/g,"");
+	Data=Data.replace(/\s/g,"");
+	Data=Data.replace(/\,/g,":");
+	Data=Data.replace(/\(/g,"");
+	alert(7+","+Data);
+	Data=Data.split(">");
+	//bord
+	tmp=[0,0,0,0];
+	for(i in Data)
+	{
+		Tmp=tmp[1];
+		tmp=Data[i].slice(6).split(":");
+		tmp[2]=Data[i].slice(1,5);//src
+		//alert(tmp);
+		exp+=tmp[2];
+		if(i%colu==colu-1)
+		{
+		exp+=",";
+		}
+	}
+	if(cnf){alert("出力完了しました。");}
+	return exp;
+
+}
+
+
+var d4="";
+function Imp(Dat,conf)//ユーザーに聞くかどうか
+{
+	if(!bord){alert("枠線が出ていないので入力できません。");return;}
+	var imp=Dat.replace(/\s　/g,"");//全角スペースを含むスペース消去
+	imp=imp.replace(/\(.*?\)/g,"");//下方互換のため()内を消去
+	var i,k;
+	var tmp="";//一時退避
+	var dat=[];//なんやかんや
+	imp=imp.split(",");
+	var j=imp[0].length/4;
+	if(j%1){alert("データに誤りがあります。");return;}//
+	if(conf)
+	{
+		if(!confirm("入力すると、現在の回路は上書きされます。よろしいですか？"))
+		{return;}
+	}
+	document.getElementById("crc").innerHTML="";
+	for(i in imp)//行ずつ
+	{
+		if(imp[i].length==0){break;}//終了
+		if(j!=imp[i].length/4){alert("データに誤りがあります。");return;}
+		Write("f",(i-(-1)),0)//行前
+		for(k=0;k<j;k++)
+		{
+			tmp=imp[i].substr(k*4,4);//4文字取り出す
+			Write(tmp,(i-(-1)),(k-(-1)));//出力
+		}
+		Write("e",(i-(-1)),0);//次行出力
+	}
+	document.getElementById("Crc").innerHTML=opn+d4+"</table>";
+	d4="";
+	colu=j;
+	rw=i;
+}
+
+function Write(srcs,rows,cols)//出力
+{
+
+	if(srcs=="f")
+	{
+		d4+='<tr id="r'+rows+'">';
+	}
+	else if(srcs=="e")
+	{
+		d4+='</tr>'
+	}
+	else
+	{
+		d4+='<td style="border: '+Bwidth+'px '+Bstyle+' '+Bcolor+
+			'" id="c'+cols+'_'+rows+'">'+'<img src="genelec.files/'+srcs+'.PNG" width=32 height=32'+
+			' onclick="Path('+cols+","+rows+')"></td>';
+	}
+
+}
